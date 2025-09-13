97bc56975f02df2ded4a075537f09d2749aa2804
ABHISHEK JYOTISHI <abhishekjyotishirooney@gmail.com>
2025-09-13 10:36:51 +0530

    Added folder structure and routing modules

---
+++ DevSync/FrontEnd/src/app/components/deployments/deployments.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Deployments } from './deployments';
+
+describe('Deployments', () => {
+  let component: Deployments;
+  let fixture: ComponentFixture<Deployments>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Deployments]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Deployments);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ DevSync/FrontEnd/src/app/components/deployments/deployments.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deployments',
+  imports: [],
+  templateUrl: './deployments.html',
+  styleUrl: './deployments.css'
+})
+export class Deployments {
+
+}
